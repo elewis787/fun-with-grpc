1e01f56c63c252a531af8c1fcfaac03a3947b65b
Ethan Lewis <elewis@kochava.com>
2017-12-04 09:48:06 -0800

    added stubs for cli apps and added zap deps

--- cmd/fwgrpc-client/main.go
+++ cmd/fwgrpc-client/main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"os"
+
+	"go.uber.org/zap"
+
+	"github.com/urfave/cli"
+)
 
 func main() {
-	fmt.Println("hello")
+	// Init the urfave cli app
+	app := cli.NewApp()
+	app.Name = "epidemic-cli"
+	app.Usage = "cli used to interact with a gRPC client"
+	app.Version = "v0.0.0"   // major,minor,patch
+	app.Flags = []cli.Flag{} // defined in flags.go
+	// ------- Main Application function -------
+	app.Action = func(cliCTX *cli.Context) error {
+		// Init zap logger
+		zlogger, err := zap.NewDevelopment() // TODO this needs to be configured for production
+		if err != nil {
+			return err
+		}
+		zlogger.Info("epic")
+		return nil
+	}
+	// Start main
+	if err := app.Run(os.Args); err != nil {
+		log.Fatal(err)
+	}
 }
